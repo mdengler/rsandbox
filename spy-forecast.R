a1250297108b6639595836154f15fa256b076345
Martin Dengler <martin@martindengler.com>
2012-05-30 13:22:17 +0800

    spy-forecast.R

---
+++ spy-forecast.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env R
+# --no-restore --no-save -f $0
+
+library(quantmod)
+getSymbols("SPY",src="google")
+getSymbols("^VIX",src="yahoo")
+
+mm <- specifyModel(Next(OpCl(SPY)) ~ OpCl(SPY) + Cl(VIX))
+
+
+library(fpp)
+fc = forecast(Cl(SPY))
+summary(fc)
