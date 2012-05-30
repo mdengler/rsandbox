f62af5638b4afe2ab0d8d10862126cd1cb1bb877
Martin Dengler <martin@martindengler.com>
2012-05-30 17:17:05 +0800

    spy-dfa.R: add log returns and poly1 vs poly2 detrends for experimentation

--- spy-dfa.R
+++ spy-dfa.R
@@ -10,8 +10,28 @@ getSymbols("^VIX",src="yahoo")
 SPY_CLOSE = Cl(SPY)
 fractalBlock = DFA(coredata(SPY_CLOSE))
 
-str(fractalBlock)
-summary(fractalBlock)
+#str(fractalBlock)
+#summary(fractalBlock)
 DFA.walk <- fractalBlock
-print DFA.walk
+print(DFA.walk)
 
+log_returns_SPY_CLOSE = diff(log(SPY_CLOSE))
+fractalBlock = DFA(coredata(log_returns_SPY_CLOSE), detrend="poly2")
+DFA.walk <- fractalBlock
+print(DFA.walk)
+
+fractalBlock = DFA(coredata(log_returns_SPY_CLOSE), detrend="poly1")
+DFA.walk <- fractalBlock
+print(DFA.walk)
+
+fractalBlock = DFA(log_returns_SPY_CLOSE, detrend="poly2")
+DFA.walk <- fractalBlock
+print(DFA.walk)
+
+fractalBlock = DFA(log_returns_SPY_CLOSE, detrend="poly1")
+DFA.walk <- fractalBlock
+print(DFA.walk)
+
+# png(filename="DFA_%03d_med.png", width=480, height=480)
+eda.plot(DFA.walk)
+# dev.off()
