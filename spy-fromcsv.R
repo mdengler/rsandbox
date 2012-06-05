68bd684df6c5b6600b40fd12be10bd0a72d9f914
Martin Dengler <martin@martindengler.com>
2012-06-05 18:54:02 +0800

    spy-fromcsv-R: initial commit

---
+++ spy-fromcsv.R
@@ -0,0 +1,15 @@
+# http://stackoverflow.com/questions/8970823/how-to-load-csv-data-file-into-r-for-use-with-quantmod
+
+library(quantmod)
+
+# create sample data
+getSymbols("SPY")
+write.zoo(SPY, file="SPY.csv", sep=",")
+
+# set symbol lookup
+setSymbolLookup(SPY=list(src="csv",format="%Y-%m-%d"))
+# call getSymbols(.csv) with auto.assign=FALSE
+spy <- getSymbols("SPY", auto.assign=FALSE)
+
+#setDefaults(chartSeries, theme="white")
+barChart(spy, theme="white")
