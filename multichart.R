5ef8262853403ab05e8d221e2b26300d7481a169
Martin Dengler <martin@martindengler.com>
2012-08-25 11:14:20 +0100

    multichart: add more symbols

--- multichart.R
+++ multichart.R
@@ -1,5 +1,5 @@
 library(quantmod)
-symbols = list("AAPL", "SPY", "000001.SS")
+symbols = list("AAPL", "^VIX", "SPY", "000001.SS", "^TNX", "^FVX", "^TYX", "XAUUSD=X", "XAGUSD=X")
 
 hash <- function( keys ) {
     result <- new.env( hash = TRUE, parent = emptyenv(), size = length( keys ) )
@@ -13,4 +13,5 @@ bars = hash(symbols)
 
 for (symbol in symbols) {
     bars[[symbol]] = getSymbols(symbol, from="1900-01-01", auto.assign=FALSE, adjust=TRUE)
+    write.zoo(bars[[symbol]], file=paste(symbol, ".csv", sep=""))
 }
