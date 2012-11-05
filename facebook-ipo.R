49fb19ac9370d9177632fe8abb29b95c3cd9891d
Martin Dengler <martin@martindengler.com>
2012-11-06 03:37:24 +0800

    facebook-ipo.R: reorder and tidyup

--- facebook-ipo.R
+++ facebook-ipo.R
@@ -1,10 +1,14 @@
-ipo = read.csv("ipo-data-nyt_from_how-the-facebook-offering-compares.csv", row.names=1)
+#!/usr/bin/env R
+# --no-restore --no-save -f $0
+
+ipo = read.csv("data/ipo-data-nyt_from_how-the-facebook-offering-compares.csv", row.names=1)
 ipo$lrMVMP = log(ipo$rMVMP)
 ipo$lrMVOP = log(ipo$rMVOP)
 attach(ipo)
 ipo.correlation_pearson = cor.test(ipo$lrMVMP, ipo$lrMVOP, method="pearson")
 ipo.correlation_spearman = cor.test(ipo$lrMVMP, ipo$lrMVOP, method="spearman")
+cor(ipo)
+symnum(cor(ipo))
 plot(ipo$lrMVMP, ipo$lrMVOP, xlab="lrMVMP", ylab="lrMVOP", pch=21)
 abline(lm(ipo$lrMVOP ~ ipo$lrMVMP))
-cor.test(ipo)
-symnum(cor(ipo))
+cor.test(ipo) #fails
