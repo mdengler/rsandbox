43b4de987175a526eb43b8e92903383e361f7ede
Martin Dengler <martin@martindengler.com>
2013-06-02 18:24:52 +0100

    facebook-ipo.R: whitespace

--- facebook-ipo.R
+++ facebook-ipo.R
@@ -2,13 +2,18 @@
 # --no-restore --no-save -f $0
 
 ipo = read.csv("data/ipo-data-nyt_from_how-the-facebook-offering-compares.csv", row.names=1)
+
 ipo$lrMVMP = log(ipo$rMVMP)
 ipo$lrMVOP = log(ipo$rMVOP)
+
 attach(ipo)
+
 ipo.correlation_pearson = cor.test(ipo$lrMVMP, ipo$lrMVOP, method="pearson")
 ipo.correlation_spearman = cor.test(ipo$lrMVMP, ipo$lrMVOP, method="spearman")
 cor(ipo)
+
 symnum(cor(ipo))
+
 plot(ipo$lrMVMP, ipo$lrMVOP, xlab="lrMVMP", ylab="lrMVOP", pch=21)
 abline(lm(ipo$lrMVOP ~ ipo$lrMVMP))
 cor.test(ipo) #fails
